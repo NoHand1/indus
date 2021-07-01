6cd2a99423ee15daa81dffc281b3adbea2b4a8cd
NoHand <55759176+NoHand1@users.noreply.github.com>
2021-07-01 08:28:32 +0200

    Add files via upload

--- dht11_example.c
+++ dht11_example.c
@@ -1,134 +1,87 @@
-#include <wiringPi.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-
-#define PIN (7)
-
-
-//Auslesen der Daten
-int ReadData(){
-
-
-int i=0, count=0;
-int data[5] = { 0 };
-int buffer[40]= { 0 };
-
-
-//Startsignal zur Uebertragung
-pinMode(PIN,OUTPUT);
-   digitalWrite(PIN,LOW);
-   delay(18);
-   digitalWrite(PIN,HIGH);
-   delayMicroseconds(30);
-pinMode(PIN,INPUT);
-
-
-//Solange warten bis Low
-while(digitalRead(PIN)==HIGH) delayMicroseconds( 1 );
-//Antwort Sensor
-
-
-while(digitalRead(PIN)==LOW) delayMicroseconds(1);
-while(digitalRead(PIN)==HIGH) delayMicroseconds( 1 );
-
-
-//Saten in Buffer schreiben
-for(i=0; i < 40; i++)
-   {
-while(digitalRead(PIN)==LOW){
-           delayMicroseconds(1);
-       }
-
-
-       //Bit high oder low
-delayMicroseconds(29);
-if(digitalRead(PIN)==LOW) buffer[i]=0;
-else if (digitalRead(PIN)==HIGH) buffer[i]=1;
-   }
-
-
-
-
-
-
-// Luftfeuchtigkeit
-data[0] = buffer[7] + buffer[6]*2 + buffer[5]*4 + buffer[4]*8 + buffer[3]*16 + buffer[2]*32 + buffer[1]*64 + buffer[0]*128;
-data[1] = buffer[15] + buffer[14]*2 + buffer[13]*4 + buffer[12]*8 + buffer[11]*16 + buffer[10]*32 + buffer[9]*64 + buffer[8]*128;
-
-
-// Temperatur
-data[2] = buffer[23] + buffer[22]*2 + buffer[21]*4 + buffer[20]*8 + buffer[19]*16 + buffer[18]*32 + buffer[17]*64 + buffer[16]*128;
-data[3] = buffer[31] + buffer[30]*2 + buffer[29]*4 + buffer[28]*8 + buffer[27]*16 + buffer[26]*32 + buffer[25]*64 + buffer[24]*128;
-
-
-// Checksumme
-data[4] = buffer[39] + buffer[38]*2 + buffer[37]*4 + buffer[36]*8 + buffer[35]*16 + buffer[34]*32 + buffer[33]*64 + buffer[32]*128;
-
-
-// Nur zum Testen
-for(i=0; i < 8; i++)
-   {
-       printf("%d", buffer[i]);
-   }
-   puts("");
-   for(i=8; i < 16; i++)
-   {
-       printf("%d", buffer[i]);
-   }
-   puts("");
-   for(i=16; i < 24; i++)
-   {
-       printf("%d", buffer[i]);
-   }
-   puts("");
-   for(i=24; i < 32; i++)
-   {
-       printf("%d", buffer[i]);
-   }
-puts("");
-
-
-printf("%d\n", data[0]);
-printf("%d\n", data[1]);
-printf("%d\n", data[2]);
-printf("%d\n", data[3]);
-printf("%d\n", data[4]);
-
-
-printf("Temperatur: %d,%d °C, Luftfeuchtigkeit: %d,%d %%\n", data[2],data[3],data[0],data[1]);
-
-
-   // Ausgabe -> vorher Checksumme überprüfen (Byte 5 == Byte 1-4)
-   /*if( data[4] == (data[0]+data[1]+data[2]+data[3]) )
-   {
-       printf("Temperatur: %d %d   Luftfeuchtigkeit: %d %d\n", data[2],data[3],data[0],data[1]);
-   }
-   else
-   {
-       printf("Daten konnten nicht ermittelt werden!\n");
-   }*/
-return EXIT_SUCCESS;
-}
-
-
-
-
-int main ()
-{
-//WiringPi initialisieren
-wiringPiSetup();
-
-
-while(1)
-   {
-       ReadData();
-       delay(2000);
-   }
-
-
-return EXIT_SUCCESS;
-}
+#include <wiringPi.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <stdint.h>
+#include <time.h>  
+#define MAX_TIME 85  
+#define DHT11PIN 7  
+int dht11_val[5]={0,0,0,0,0};  
+  
+void dht11_read_val()  
+{  
+  uint8_t lststate=HIGH;  
+  uint8_t counter=0;  
+  uint8_t j=0,i;  
+  for(i=0;i<5;i++)  
+     dht11_val[i]=0;  
+  pinMode(DHT11PIN,OUTPUT);  
+  digitalWrite(DHT11PIN,LOW);  
+  delay(18);  
+  digitalWrite(DHT11PIN,HIGH);  
+  delayMicroseconds(40);  
+  pinMode(DHT11PIN,INPUT);  
+  for(i=0;i<MAX_TIME;i++)  
+  {  
+    counter=0;  
+    while(digitalRead(DHT11PIN)==lststate){  
+      counter++;  
+      delayMicroseconds(1);  
+      if(counter==255)  
+        break;  
+    }  
+    lststate=digitalRead(DHT11PIN);  
+    if(counter==255)  
+       break;  
+    // top 3 transistions are ignored  
+    if((i>=4)&&(i%2==0)){  
+      dht11_val[j/8]<<=1;  
+      if(counter>16)  
+        dht11_val[j/8]|=1;  
+      j++;  
+    }  
+  }  
+  // verify cheksum and print the verified data  
+  if((j>=40)&&(dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF)))  
+  {  
+    printf("Humidity = %d.%d %% Temperature = %d.%d°C\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
+    char text[100];
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    
+    
+    strftime(text, sizeof(text)-1, "%d-%m-%Y", t);
+    char prefix[50] = "/var/www/log-";
+    strcat(prefix,text);
+    char suffix[50] = ".csv";
+    strcat(prefix,suffix);
  
-	return(0);
+    FILE *fp;
+    fp=fopen(prefix,"a");
+    fprintf(fp,"%u000,%d%d,%d\n",(unsigned)time(NULL),dht11_val[2],dht11_val[3],dht11_val[0]);
+    fclose(fp);
+    FILE *fh;
+    fh=fopen("/var/www/current_hyg.csv", "w");
+    fprintf(fh,"%d\n",dht11_val[0]);
+    fclose(fh);
+    FILE *ft;
+    ft=fopen("/var/www/current_temp.csv", "w");
+    fprintf(ft,"%d.%d\n",dht11_val[2],dht11_val[3]);
+    fclose(ft);
+    exit(1);  
+  }  
+  else  
+    printf("Invalid Data!!\n");  
+}  
+  
+int main(void)  
+{  
+  printf("Interfacing Temperature and Humidity Sensor (DHT11) With Banana Pi\n");  
+  if(wiringPiSetup()==-1)  
+    exit(1);  
+  while(1)  
+  {  
+     dht11_read_val();
+     delay(3000);  
+  }  
+  return 0;  
 }
